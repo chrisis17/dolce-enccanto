9ad67cdb7ab10ba1518410ea320ca595282876a7
Christian Rojas <christian.rojas@utec.edu.pe>
2022-11-28 23:30:31 -0500

    catalogo done

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ typewriter
     .typeString('Av.tal numero tal')
     .pauseFor(200)
     .start();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration: 1,
+        dist: -80,
+        shift: 5,
+        padding: 5,
+        numVisible: 3,
+        indicators: true,
+        noWrap: false
+    });
+});
